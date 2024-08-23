a100517c2b26f8114d046896d15e7ba48db50b8f
frysc2 <jat1213@hotmail.com>
2024-08-23 14:37:50 -0700

    Update TranscriptScraper.R

--- TranscriptScraper.R
+++ TranscriptScraper.R
@@ -4,13 +4,16 @@ library(rvest)
 webfile <- "https://futurama.fandom.com/wiki/Futurama_Wiki"
 webpage <- read_html( webfile)
 
-
+###########
+#### Getting Futurama Episode list
+###########
 SeasonsOversheet4 <-  webpage %>%
   
   html_nodes(xpath = "/html/body/div[4]/div[4]/div[2]/header/nav/ul/li[4]/div[2]/ul/li/div/ul/li/a") %>%
   html_attr("href") %>%
   str_trim()
 
+data.frame(SeaonTables_links)
 
 SeasonDataTable_list <- data.frame()
 for (i in 1:5) {
@@ -67,3 +70,45 @@ for (i in 1:5) {
     SeasonDataTable_list <- rbind(SeasonDataTable_list, SeasonDataTable)
     print(i)
   }
+  
+  
+  SeasonDataTable_list$Title <- gsub("[\"/]", "", SeasonDataTable_list$Title)
+  
+  SeasonDataTable_list$URL <- paste0("https://futurama.fandom.com/wiki/", str_replace_all(SeasonDataTable_list$Title," ", "_"))
+
+###########
+#### Transcript Extraction
+###########
+  
+
+  webfile <- paste0(SeasonDataTable_list$URL[1], "/Transcript")
+  webpage <- read_html(webfile)  
+  
+  Transcript <-  webpage %>%
+    html_nodes(xpath = '//*[@id="mw-content-text"]/div') %>%
+    html_text()
+
+  transcipt_data<-data.frame(str_split(Transcript, "\n") ) 
+  colnames(transcipt_data) <- "raw"
+  
+  sceneCheck <- data.frame(str_split_fixed(transcipt_data$raw, "[()]", 6))
+  colnames(sceneCheck) <- c("Dialog_1", "Scene_1",  "Dialog_2", "Scene_2", "Dialog_3", "Scene_3")
+  transcipt_data<- cbind(transcipt_data, sceneCheck)
+  
+  actionCheck <- data.frame(str_split_fixed(transcipt_data$raw, "[\\[\\]]", 6))
+  colnames(actionCheck) <- c("Dialog_1", "action_1",  "Dialog_2", "action_2", "Dialog_3", "action_3")
+  transcipt_data<- cbind(transcipt_data, actionCheck)
+  
+  
+  NoScenes <- data.frame(str_split_fixed(transcipt_data$raw, "[\\[\\]()]", 6))
+  colnames(NoScenes) <- c("Dialog_1", "action_1",  "Dialog_2", "action_2", "Dialog_3", "action_3")
+  transcipt_data$NoScenes <- paste(NoScenes$Dialog_1, NoScenes$Dialog_2, NoScenes$Dialog_3, sep = " ")
+  transcipt_data$character<-NA
+  transcipt_data$character[grep(":",transcipt_data$NoScenes)] <- str_split_fixed(transcipt_data$NoScenes[grep(":",transcipt_data$NoScenes)], ":", 2)[,1]
+  
+  table2<-data.frame(table(transcipt_data$character))
+
+########
+### Something else
+  
+  
